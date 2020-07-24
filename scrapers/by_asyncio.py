4093fa73af54b9cfca9abdde61e73d83668f8fe5
xpy <xu_py311@outlook.com>
2020-07-24 11:28:00 +0800

    - refactored, applied aiofiles

--- scrapers/by_asyncio.py
+++ scrapers/by_asyncio.py
@@ -1,35 +1,43 @@
 import asyncio
 import aiohttp
+import aiofiles
 from bs4 import BeautifulSoup
 
 from scrapers.config import BASE_URL, PAGE_URLS
-from scrapers.scraper import is_pdf_tag, save_pdf, get_pdf_links_and_names
+from scrapers.scraper import is_target_pdf_tag, gen_path, get_pdf_links_and_names
 from scrapers.utils import timer
 
 
+async def get_content_asnyc(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            content = await resp.content.read()
+    return content
+
 # async def get_pdf_links_and_names(url):
-#     resp = await aiohttp.ClientSession().get(url)
 #
 #     soup = BeautifulSoup(html, 'lxml')
 #     tags = soup.findAll('a')
 #     pdf_links_and_names = [(BASE_URL + tag['href'].lstrip('.'), tag.text)
-#                            for tag in tags if is_pdf_tag(tag)]
+#                            for tag in tags if is_target_pdf_tag(tag)]
 #     return pdf_links_and_names
+
+
+async def save_pdf_async(pdf, path):
+    async with aiofiles.open(path, 'wb') as fp:
+        await fp.write(pdf)
 
 
 async def download_one(args):
     pdf_link, pdf_name = args
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.get(pdf_link) as resp:
-            pdf = await resp.content.read()
-            
-    save_pdf(pdf, pdf_name)
+    path = gen_path(pdf_name)
+    pdf = await get_content_asnyc(pdf_link)
+    await save_pdf_async(pdf, path)
 
- 
+
 @timer
-def download_one_page(url):
-    pdf_links_and_names = get_pdf_links_and_names(url)
+def download_one_page(page_link):
+    pdf_links_and_names = get_pdf_links_and_names(page_link)
     loop = asyncio.get_event_loop()
     to_do = [download_one(args) for args in pdf_links_and_names]
     wait_coro = asyncio.wait(to_do)
@@ -37,7 +45,9 @@ def download_one_page(url):
     loop.close()
     return len(res)
 
+# def download_many()
+
 
 if __name__ == '__main__':
-    url = PAGE_URLS().__next__()
-    download_one_page(url)
+    page_link = PAGE_URLS().__next__()
+    download_one_page(page_link)
--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -5,53 +5,58 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_pdf(pdf_link):
-    resp = requests.get(pdf_link)
+def get_content(url):
+    resp = requests.get(url)
     return resp.content
 
 
-def save_pdf(pdf, file_name):
+def gen_path(file_name):
     path = ''
     if '环境卫星' in file_name:
         path += '/环境卫星'
     elif '气象卫星' in file_name:
         path += '/气象卫星'
-    else:
-        return
-    
+
     if '日报' in file_name:
         path += '/日报'
     elif '月报' in file_name:
         path += '/月报'
-    else:
-        return
+        
     path = os.path.join(DEST_DIR + path, file_name + '.pdf')
+    return path
     
+
+def save_pdf(pdf, path):
     with open(path, 'wb') as fp:
         fp.write(pdf)
- 
- 
-def is_pdf_tag(tag):
+
+
+def is_target_file(file_name):
+    return ('日报' in file_name or '月报' in file_name) and ('环境卫星' in file_name or '气象卫星' in file_name)
+
+
+def is_target_pdf_tag(tag):
     try:
-        return tag['href'].endswith('pdf')
+        return tag['href'].endswith('pdf') and is_target_file(tag.text)
     except KeyError:
         return False
 
 
-def get_pdf_links_and_names(url):
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.content, 'lxml')
+def get_pdf_links_and_names(page_link):
+    html = get_content(page_link)
+    soup = BeautifulSoup(html, 'lxml')
     tags = soup.findAll('a')
     pdf_links_and_names = [(BASE_URL + tag['href'].lstrip('.'), tag.text)
-                           for tag in tags if is_pdf_tag(tag)]
+                           for tag in tags if is_target_pdf_tag(tag)]
     return pdf_links_and_names
 
 
-def download_single_page(page_url):
-    pdf_links_and_names = get_pdf_links_and_names(page_url)
+def download_single_page(page_link):
+    pdf_links_and_names = get_pdf_links_and_names(page_link)
     for pdf_link, pdf_name in pdf_links_and_names:
-        pdf = get_pdf(pdf_link)
-        save_pdf(pdf, pdf_name)
-    print(f"page {page_url} downloaded!")
+        pdf = get_content(pdf_link)
+        path = gen_path(pdf_name)
+        save_pdf(pdf, path)
+    print(f"page {page_link} downloaded!")
     
 
